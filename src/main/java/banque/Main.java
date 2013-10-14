89fcd948b421d812d5f7b6d622d39c852af84ba3
djae138 <djae138@gmail.com>
2013-10-14 17:50:44 +0000

    a

---
+++ src/main/java/banque/Main.java
@@ -0,0 +1,14 @@
+package banque;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int addition(int a, int b){
+		return (a+b);
+	}
+
+}
